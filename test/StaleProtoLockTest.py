186079981eb6e6a0757ffd76c22f65f5fc723a5f
dywalsh <dywalsh@arista.com>
2021-06-29 08:53:37 -0900

    IpRoute service manager proto file for EosSdkRpc
    IpRouteMgrService template proto file for EosSdkRpc
    
    Proto.lock has not been updated to allow for changes to be made during server-side implementation. 
    BUG 594564 created to ensure the lock file is updated before the end of the EosSdk Hardening project.
    
    http://reviewboard/r/307189
    fixes=BUG597358
    integrate all member packages from dywalsh.iprouteMgrProto.0@22930677 (launched at change 22809593)
    eos-trunk@22936799 autobuild 13316336
    autosubmitted=1

--- test/StaleProtoLockTest.py
+++ test/StaleProtoLockTest.py
@@ -12,12 +12,22 @@ import os
 # The proto.lock file is used to ensure, no compatibility breaking changes are
 # made to the proto files.
 
+# Remove files after excluded protobufs are finalised.
+# Used to prevent build failures due to not updating the lock file
+# for template protobufs.
+excludedFiles = [
+   'ip_route.proto',
+   'defs.proto',
+   ]
+
 def CheckLockFileIsNotStale():
    lockFileModifiedDate = os.path.getmtime( '/src/EosSdkRpcProtos/proto/proto.lock' )
    protoFiles = glob.glob( "/src/EosSdkRpcProtos/proto/*.proto" )
    # Find the latest modified proto file
    protoFilesModifiedDate = max( [ os.path.getmtime( path )
-                                   for path in protoFiles ] )
+                                   for path in protoFiles
+                                   if os.path.basename( path )
+                                   not in excludedFiles ] )
    if protoFilesModifiedDate > lockFileModifiedDate:
       raise AssertionError( "Proto.lock has not been updated to include the "
             "changes made to the protobuf files.\n"
