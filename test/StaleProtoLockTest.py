847e443722c5dd8af43c7e38d64cb37fe9d93c7a
dywalsh <dywalsh@arista.com>
2021-07-08 22:20:06 -0900

    IntfCounter service manager proto file for EosSdkRpc
    IntfCounterService template proto file for EosSdkRpc
    
    Proto.lock has not been updated to allow for changes to be made during server-side implementation. 
    BUG 594564 created to ensure the lock file is updated before the end of the EosSdk Hardening project.
    
    http://reviewboard/r/308825
    fixes=BUG597359
    integrate all member packages from dywalsh.intfCounterMgrProto.0@23096575 (launched at change 23067312)
    eos-trunk@23104541 autobuild 13357532
    autosubmitted=1

--- test/StaleProtoLockTest.py
+++ test/StaleProtoLockTest.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import, division, print_function
 
 import glob
 import os
+import Tac
 
 # This test ensures that any .proto files within /src/EosSdkRpcProtos/proto
 # have been modified without updating the proto.lock file.
@@ -18,29 +19,33 @@ import os
 excludedFiles = [
    'bgp.proto',
    'bgp_path.proto',
-   'ip_route.proto',
    'defs.proto',
+   'eapi.proto',
    'intf.proto',
-   'macsec.proto',
    'ip_intf.proto',
+   'ip_route.proto',
+   'macsec.proto',
+   'EosSdkRpc.proto',
    ]
 
 def CheckLockFileIsNotStale():
    lockFileModifiedDate = os.path.getmtime( '/src/EosSdkRpcProtos/proto/proto.lock' )
    protoFiles = glob.glob( "/src/EosSdkRpcProtos/proto/*.proto" )
    # Find the latest modified proto file
-   protoFilesModifiedDate = max( [ os.path.getmtime( path )
-                                   for path in protoFiles
-                                   if os.path.basename( path )
-                                   not in excludedFiles ] )
-   if protoFilesModifiedDate > lockFileModifiedDate:
-      raise AssertionError( "Proto.lock has not been updated to include the "
-            "changes made to the protobuf files.\n"
-            "More protolock info in README.\n"
-            "Please run:\n"
-            "a4 open /src/EosSdkRpcProtos/proto/proto.lock \n"
-            "protolock commit -lockdir /src/EosSdkRpcProtos/proto"
-            " -protoroot /src/EosSdkRpcProtos/proto" )
+   protoFileTimestamps = [ os.path.getmtime( path )
+                           for path in protoFiles
+                           if os.path.basename( path )
+                           not in excludedFiles ]
+   if protoFileTimestamps:
+      protoFilesModifiedDate = max( protoFileTimestamps )
+      if protoFilesModifiedDate > lockFileModifiedDate:
+         raise AssertionError( "Proto.lock has not been updated to include the "
+               "changes made to the protobuf files.\n"
+               "More protolock info in README.\n"
+               "Please run:\n"
+               "a4 open /src/EosSdkRpcProtos/proto/proto.lock \n"
+               "protolock commit -lockdir /src/EosSdkRpcProtos/proto"
+               " -protoroot /src/EosSdkRpcProtos/proto" )
 
 if __name__ == '__main__':
    CheckLockFileIsNotStale()
