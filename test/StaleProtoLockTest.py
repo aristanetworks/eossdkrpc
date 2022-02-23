c2ea1d45b838a78f057dec806fbc53bf7e4a881f
paulo.panhoto <paulo.panhoto@arista.com>
2022-02-23 04:41:48 -0800

    Updated StaleProtoLockTest and removed hash file
    Change-Id: I5f2bab956b4f00c1cbebde58ad717fcf445b35c6

--- test/StaleProtoLockTest.py
+++ test/StaleProtoLockTest.py
@@ -2,53 +2,52 @@
 # Copyright (c) 2021 Arista Networks, Inc.  All rights reserved.
 # Arista Networks, Inc. Confidential and Proprietary.
 
+'''
+This test ensures that any .proto files within /src/EosSdkRpcProtos/proto
+have been modified without updating the proto.lock file.
+The proto.lock file is used to ensure, no compatibility breaking changes are
+made to the proto files.
+
+A new proto.lock file, created in a temporary directory, to be matched against
+the provided (committed) proto.lock in the directory proto. Both must contain
+the same information.
+
+To prevent against ordering issues, `DeepDiff` of the parsed
+json objects is used instead of plain diff between the files.
+'''
 from __future__ import absolute_import, division, print_function
 
 import Assert
-import glob
-from hashlib import sha256
+from deepdiff import DeepDiff
+import json
+import os
+import tempfile
 
-# This test ensures that any .proto files within /src/EosSdkRpcProtos/proto
-# have been modified without updating the proto.lock file.
-# The proto.lock file is used to ensure, no compatibility breaking changes are
-# made to the proto files.
-
-# Remove files after excluded protobufs are finalised.
-# Used to prevent build failures due to not updating the lock file
-# for template protobufs.
 excludedFiles = []
 sourceDir = '/src/EosSdkRpcProtos/proto/'
 
-def getHashes():
-   with open( sourceDir + 'protolock.shasum', 'r' ) as hashFile:
-      hashes = {}
-      for line in hashFile:
-         hashStr, fileName = line.split()
-         if fileName in excludedFiles:
-            continue
-         hashes[ fileName ] = hashStr
-      return hashes
+def createLockFile():
+   tempDir = tempfile.mkdtemp()
+   lockFile = tempDir + '/proto.lock'
+   print( 'Creating temporary lock file {}'.format( lockFile ) )
+   Assert.assertEqual(
+      os.system( 'protolock init --protoroot {} --lockdir {}'.format(
+         sourceDir, tempDir ) ), 0 )
+   return lockFile
 
-def matchHashes( hashes ):
-   fail = False
-   for name in hashes:
-      expected = hashes[ name ]
-      name = sourceDir + name
-      with open( name, 'rb' ) as testFile:
-         current = sha256( testFile.read() ).hexdigest()
-      if current != expected:
-         print( "hash check failed for proto file {}".format( name ) )
-         fail = True
-   protoFiles = glob.glob( sourceDir + "*.proto" )
-   for protoFile in protoFiles:
-      if not protoFile[ len( sourceDir ) : ] in hashes:
-         print( "proto file {} not hashed".format( protoFile ) )
-         fail = True
-   Assert.assertFalse( fail,
-      "Some errors occurred. Please run the script "
+def verifyProtolock():
+   refFile = createLockFile()
+   sourceFile = sourceDir + 'proto.lock'
+   testData = json.load( open( sourceFile, 'r' ) )
+   referenceData = json.load( open( refFile, 'r' ) )
+   diff = DeepDiff( testData, referenceData, ignore_order=True,
+      report_repetition=True )
+   if diff:
+      os.system( 'diff -u {} {}'.format( sourceFile, refFile ) )
+      Assert.fail( "Some errors occurred. Please run the script "
       "/src/EosSdkRpcProtos/checkprotos to verify and maintain the protolock "
-      "metadata and hashes up to date.\n"
+      "metadata up to date.\n"
       "See README.md for more details" )
 
 if __name__ == '__main__':
-   matchHashes( getHashes() )
+   verifyProtolock()
